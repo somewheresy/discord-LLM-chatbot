aee42e3113321424ccd008268c24e43092624b4a
somewheresy <43761886+somewheresy@users.noreply.github.com>
2023-06-03 20:14:40 +0000

    stable

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ import re
 import json
 import logging
 
-# Assistant
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 scraperapi_key = os.getenv("SCRAPER_API_KEY")
+brave_search_api_key = os.getenv("BRAVE_SEARCH_API_KEY")
 intents = discord.Intents.default()
 bot = commands.Bot("/", intents=intents)
 
@@ -41,13 +41,13 @@ def get_current_date():
   return str(time.strftime("%Y-%m-%d, %H:%M:%S"))
 
 
-def openAIGPTCall(messages, model="gpt-3.5-turbo"):
+def openAIGPTCall(messages, model="gpt-4", temperature=1.0):
   start_time = time.time()
   response = openai.ChatCompletion.create(model=model,
                                           messages=messages,
-                                          temperature=1.0)
+                                          temperature=temperature)
   elapsed_time = (time.time() - start_time) * 1000
-  cost_factor = 0.002
+  cost_factor = 0.04
   cost = cost_factor * (response.usage["total_tokens"] / 1000)
   message = response.choices[0].message.content.strip()
   return message, cost, elapsed_time
@@ -87,7 +87,7 @@ async def check_job_status(job_id, channel):
               "role": "system",
               "content": system_prompt
             }])
-            await channel.send(summary)
+            await send_long_message(channel, summary)
             break
           except json.JSONDecodeError:
             logging.error("Failed to decode JSON from result response")
@@ -96,6 +96,15 @@ async def check_job_status(job_id, channel):
         else:
           logging.error("Empty response received from result URL")
           break
+
+
+async def send_long_message(channel, message):
+  max_length = 2000
+  chunks = [
+    message[i:i + max_length] for i in range(0, len(message), max_length)
+  ]
+  for chunk in chunks:
+    await channel.send(chunk)
 
 
 @bot.event
@@ -118,6 +127,25 @@ async def on_message(message):
       dialog_contexts[message.author.id] = DialogContext()
       dialog_contexts[message.author.id].add_message("system", system_prompt)
 
+    # Parse temperature
+    temp_match = re.search(r'::temperature=([0-9]*\.?[0-9]+)::', user_prompt)
+    temperature = 1.0  # default temperature
+    if temp_match:
+      temperature = float(temp_match.group(1))
+      user_prompt = re.sub(
+        r'::temperature=([0-9]*\.?[0-9]+)::', '',
+        user_prompt)  # remove temperature command from user_prompt
+
+    # Parse model
+    model_match = re.search(r'::model=(\w+[-]*\w+\.?\w*)::', user_prompt)
+    model = "gpt-3.5-turbo"  # default model
+    if model_match:
+      model = model_match.group(1)
+      user_prompt = re.sub(
+        r'::model=(\w+[-]*\w+\.?\w*)::', '',
+        user_prompt)  # remove model command from user_prompt
+
+    # Parse URL for scraping
     url_match = re.search(r'\bhttps?://\S+\b', user_prompt)
     if url_match:
       url = url_match.group()
@@ -132,15 +160,51 @@ async def on_message(message):
         await message.channel.send(response)
       return
 
+    # Parse search query
+    search_match = re.search(r'::search\s(.*)::', user_prompt)
+    if search_match:
+      search_query = search_match.group(1)
+      headers = {
+        "Accept": "application/json",
+        "X-Subscription-Token": brave_search_api_key
+      }
+      response = requests.get(
+        f"https://api.search.brave.com/res/v1/web/search?q={search_query}",
+        headers=headers)
+      if response.status_code == 200:
+        search_results = response.json()['web']
+
+        print(f"Search results: {search_results}")
+
+        # Here, we feed the search results into the summarizer
+        # Here, we feed the search results into the summarizer
+        for result in search_results:
+          logging.info(result)
+          logging.info("summarizing...")
+          url = result.get('url')
+          if url:
+            _, scraped_content = scrape_and_summarize(url)
+            if scraped_content:
+              system_prompt = "Please provide a brief summary of the following content: " + str(
+                scraped_content)
+              summary, _, _ = openAIGPTCall([{
+                "role": "system",
+                "content": system_prompt
+              }])
+              await send_long_message(message.channel, summary)
+        return
+
     dialog_contexts[message.author.id].add_message("user", user_prompt)
 
     ai_message, cost, elapsed_time = openAIGPTCall(
-      dialog_contexts[message.author.id].get_messages())
+      dialog_contexts[message.author.id].get_messages(),
+      model=model,
+      temperature=temperature)
     logging.info(f"Generated AI message: {ai_message}")
     logging.info(f"AI message cost: {cost}, elapsed time: {elapsed_time}")
     dialog_contexts[message.author.id].add_message("assistant", ai_message)
 
-    await message.channel.send(ai_message)
+    await send_long_message(message.channel, ai_message)
 
 
 bot.run(os.getenv('DISCORD_TOKEN'))
